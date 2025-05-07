2a7ea824cd7937aa02053228c7134c02b582c9c6
NicholasLe04 <nicholasl.le224@gmail.com>
2025-05-07 14:18:03 -0700

    Remove nginx

--- backend/main.py
+++ backend/main.py
@@ -18,12 +18,12 @@ from routers.v1 import inventory
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://frontend:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.include_router(inventory.router, prefix="/v1/inventory")
+app.include_router(inventory.router, prefix="/api/v1/inventory")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,8 +8,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true
+        target: 'http://backend:8000',
+        changeOrigin: true,
+        secure: false,
       }
     }
   }
