061ed750da75d64b4fde52c2d0eb950365784ea7
NicholasLe04 <nicholasl.le224@gmail.com>
2025-05-07 16:57:53 -0700

    Add dev vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,17 +2,33 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/eats/',
+// Base config shared between dev and prod
+const baseConfig = {
   plugins: [react(), tailwindcss()],
+}
+
+// Dev config
+const devConfig = {
+  ...baseConfig,
+  base: '/',
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:6969',
         changeOrigin: true
       }
     },
     allowedHosts: ['one.sce']
   }
+}
+
+// Prod config
+const prodConfig = {
+  ...baseConfig,
+  base: '/eats',
+}
+
+// https://vite.dev/config/
+export default defineConfig(({ command }) => {
+  return command === 'serve' ? devConfig : prodConfig
 })
