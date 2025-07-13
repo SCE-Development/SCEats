92512520c8bccce168215dfb42f3b4822c68a429
adarshm11 <amlearning2023@gmail.com>
2025-07-12 21:56:11 -0700

    added two small metrics

--- backend/routers/v1/inventory.py
+++ backend/routers/v1/inventory.py
@@ -29,12 +29,27 @@ from utils.db import (
     update_snack,
     create_bulk_items
 )
+import prometheus_client
 
 router = APIRouter()
+
+# Prometheus metrics
+# track count of different types of snacks
+snack_count = prometheus_client.Gauge(
+    "snack_count",
+    "Amount of snack types in inventory",
+)
+
+# track count of purchases
+purchase_count = prometheus_client.Counter(
+    "purchase_count",
+    "Total number of snacks bought from inventory",
+)
 
 @router.get("/", response_model=InventoryResponse)
 async def get_inventory_route():
     snacks = get_inventory()
+    snack_count.set(len(snacks)) # set the counter to the current number of snacks
     return { "snacks": snacks }
 
 @router.get("/snacks/{sku}", response_model=Snack)
@@ -44,6 +59,7 @@ async def get_snack_route(sku: str):
 
 @router.post("/snacks", response_model=Snack)
 async def create_snack_route(snack: SnackCreateSchema):
+    snack_count.inc()
     return create_snack(snack)
 
 @router.put("/snacks/{sku}", response_model=Snack)
@@ -55,10 +71,12 @@ async def purchase_snack_route(request:PurchaseRequest):
     for purchase_request in request.purchase_requests:
         snack=get_snack(purchase_request.sku)
         update_snack(purchase_request.sku, SnackUpdateSchema(quantity=max(0,snack.quantity - purchase_request.quantity)))
+        purchase_count.inc()
     return PurchaseResponse(success=True,message="Purchase successful",purchase_requests=request.purchase_requests) 
 
 @router.delete("/snacks/{sku}", response_model=Snack)
 async def delete_snack_route(sku: str):
+    snack_count.dec(1)
     return delete_snack(sku)
 
 @router.post("/snacks/bulk", response_model=BulkSnackResponse) 
